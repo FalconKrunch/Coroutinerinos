b8992f264bb334e85965f3229e5d65451ebade0d
FalconKrunch <Vicinalace@gmail.com>
2015-07-13 04:31:26 +0200

    Initial Commit

---
+++ CoroutineSystem/CoroutineSystem/CoroutineSystem.cs
@@ -0,0 +1,120 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) 2015 Joppe van der Meij
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Coroutines
+{
+	
+	public class Message
+	{
+
+	}
+	public delegate void MessageDelegate();
+	public class MessageMapper
+	{
+		public Dictionary<Type, MessageDelegate> mappedMessages = new Dictionary<Type, MessageDelegate>();
+	}
+	public class Coroutine
+	{
+		[Flags]
+		public enum Attributes
+		{
+			Looping = 1,
+		}
+		public Coroutine()
+		{
+			curEnumeration = function().GetEnumerator();
+		}
+		public virtual IEnumerable function() { return "Empty routine"; }
+		public Attributes attributes;
+		public IEnumerator curEnumeration;
+		public MessageMapper mapper = new MessageMapper();
+		~Coroutine()
+		{
+
+		}
+	}
+
+	class CoroutineSystem
+	{
+		public Stack<Coroutine> coroutines = new Stack<Coroutine>();
+		private bool shouldSwitch = false;
+		public void AddCoroutine(Coroutine coroutine)
+		{
+			coroutines.Push(coroutine);
+		}
+		public void Send(Type message)
+		{
+			for (int i = 0; i < coroutines.Count; i++)
+			{
+				Coroutine coroutine = coroutines.ElementAt(i);
+				if (coroutine.mapper.mappedMessages.ContainsKey(message)) //if this coroutine contains the message
+				{
+					for (int j = 0; j < i; j++)//pop all child coroutines off the stack
+					{
+						coroutines.Pop();
+					}
+					MessageDelegate del;
+					coroutine.mapper.mappedMessages.TryGetValue(message, out del);
+					del(); //call function attached to mapped message
+				}
+			}
+		}
+		public void Update()
+		{
+			shouldSwitch = false;
+			if (coroutines.Count == 0) //early out
+				return;
+			IEnumerator curEnumeration = coroutines.Peek().curEnumeration; //grab the current function position
+			while (curEnumeration.MoveNext()) //move to next yield
+			{
+				var value = curEnumeration.Current; //accept any return value, this is because we don't want a InvalidCastException
+				Type type = value.GetType();
+				if (type.IsSubclassOf(typeof(Coroutine))) //if type is a subclass of coroutine, add it to the stack and set switch flag
+				{
+					coroutines.Push((Coroutine)value);
+					shouldSwitch = true;
+					break;
+				}
+				else //print any uncaught values
+				{
+					Console.WriteLine("Uncaught type " + value.GetType().ToString() + ": " + value);
+				}
+			}
+			if (!shouldSwitch)
+			{
+				if (coroutines.Peek().attributes.HasFlag(Coroutine.Attributes.Looping)) //reset enumerator and prevent coroutine from being popped
+				{
+					coroutines.Peek().curEnumeration = coroutines.Peek().function().GetEnumerator();
+					return;
+				}
+				coroutines.Pop();
+			}
+		}
+
+	}
+
+}
---
+++ CoroutineSystem/Demo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using Coroutines;
+
+
+namespace Program
+{
+	class StopMessage : Message
+	{
+
+	}
+	class TestMath
+	{
+		public static int Fibonacci(int n)
+		{
+			int a = 0;
+			int b = 1;
+			// In N steps compute Fibonacci sequence iteratively.
+			for (int i = 0; i < n; i++)
+			{
+				int temp = a;
+				a = b;
+				b = temp + b;
+			}
+			return a;
+		}
+	}
+	class TestFunc : Coroutine
+	{
+		int int1;
+		string string1;
+		void Stop()
+		{
+			Console.Write("MessageReceived\n"); 
+		}
+
+		public TestFunc(int argInt, string argString) 
+		{
+			int1 = argInt;
+			string1 = argString;
+			mapper.mappedMessages.Add(typeof(StopMessage), new MessageDelegate(Stop)); //link Stop function to the StopMessage, these could be any type or name
+			attributes |= Attributes.Looping; //Make this coroutine loop when it ends
+		}
+		override public IEnumerable function()
+		{
+			//returning strings and integers do nothing, they just serve demonstration purposes.
+			yield return 1;
+			yield return int1;
+			yield return new TestFunc2(); //Start running TestFunc2 from this point
+			yield return string1; 
+			yield return 15;
+		}
+	}
+
+	class TestFunc2 : Coroutine
+	{
+		public TestFunc2()
+		{
+			attributes |= Attributes.Looping;
+		}
+		override public IEnumerable function()
+		{
+			int fib1 = TestMath.Fibonacci(14);
+			int fib2 = TestMath.Fibonacci(13);
+			yield return TestMath.Fibonacci(12);
+			yield return fib2;
+			yield return fib1;
+			yield return "Hello from testFunction2";
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CoroutineSystem system = new CoroutineSystem(); //create the coroutine system
+			Coroutine test = new TestFunc(65535, "Hi from Main through TestFunc"); //create initial coroutine, I recommend setting the first coroutine to loop
+			system.AddCoroutine(test); //add the coroutine to the system as entry point
+			int c = 0;
+
+			while (true)
+			{
+				c++;
+				system.Update();
+				if (c % 10 == 0) //send stop message every 10 updates
+					system.Send(typeof(StopMessage));
+			}
+		}
+	}
+}
